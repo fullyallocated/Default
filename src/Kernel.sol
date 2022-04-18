38eff461210f57eebf91e4d8f60a08d2c1aca9c9
indigo <6822158+ind-igo@users.noreply.github.com>
2022-04-18 18:37:16 -0400

    init kernel

---
+++ src/Kernel.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: AGPL-2.0
+pragma solidity ^0.8.10;
+
+contract Module {
+    error Module_OnlyApprovedPolicy(address caller_);
+
+    Kernel public _kernel;
+
+    constructor(Kernel kernel_) {
+        _kernel = kernel_;
+    }
+
+    function KEYCODE() external pure virtual returns (bytes3) {}
+
+    modifier onlyPolicy() {
+        if (_kernel.approvedPolicies(msg.sender) != true)
+            revert Module_OnlyApprovedPolicy(msg.sender);
+        _;
+    }
+}
+
+contract Policy {
+    error Policy_ModuleDoesNotExist(bytes3 keycode_);
+    error Policy_OnlyKernel(address caller_);
+
+    Kernel public _kernel;
+
+    constructor(Kernel kernel_) {
+        _kernel = kernel_;
+    }
+
+    function requireModule(bytes3 keycode_) internal view returns (address) {
+        address moduleForKeycode = _kernel.getModuleForKeycode(keycode_);
+
+        if (moduleForKeycode != address(0))
+            revert Policy_ModuleDoesNotExist(keycode_);
+
+        return moduleForKeycode;
+    }
+
+    function configureModules() external virtual onlyKernel {}
+
+    modifier onlyKernel() {
+        if (msg.sender != address(_kernel))
+            revert Policy_OnlyKernel(msg.sender);
+        _;
+    }
+}
+
+enum Actions {
+    InstallModule,
+    UpgradeModule,
+    ApprovePolicy,
+    TerminatePolicy,
+    ChangeExecutor
+}
+
+struct Instruction {
+    Actions action;
+    address target;
+}
+
+contract Kernel {
+    error Kernel_OnlyExecutive(address caller_);
+    error Kernel_KernelAlreadyLaunched(); // TODO
+    error Kernel_ModuleAlreadyInstalled(bytes3 module_);
+    error Kernel_ModuleAlreadyExists(bytes3 module_);
+    error Kernel_PolicyAlreadyApproved(address policy_);
+    error Kernel_PolicyNotApproved(address policy_);
+
+    address public executive;
+
+    constructor() {
+        executive = msg.sender;
+    }
+
+    modifier onlyExecutive() {
+        if (msg.sender != executive) revert Kernel_OnlyExecutive(msg.sender);
+        _;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    //                                 DEPENDENCY MANAGEMENT                             //
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+    mapping(bytes3 => address) public getModuleForKeycode; // get contract for module keycode
+    mapping(address => bytes3) public getKeycodeForModule; // get module keycode for contract
+    mapping(address => bool) public approvedPolicies; // whitelisted apps
+    address[] public allPolicies;
+
+    event ActionExecuted(Actions action_, address target_);
+
+    function executeAction(Actions action_, address target_)
+        external
+        onlyExecutive
+    {
+        if (action_ == Actions.InstallModule) {
+            _installModule(target_);
+        } else if (action_ == Actions.UpgradeModule) {
+            _upgradeModule(target_);
+        } else if (action_ == Actions.ApprovePolicy) {
+            _approvePolicy(target_);
+        } else if (action_ == Actions.TerminatePolicy) {
+            _terminatePolicy(target_);
+        } else if (action_ == Actions.ChangeExecutor) {
+            // require Proxy to install the executive module before calling ChangeExecutive on it
+            if (getKeycodeForModule[target_] != "EXC")
+                revert Kernel_OnlyExecutive(target_);
+
+            executive = target_;
+        }
+
+        emit ActionExecuted(action_, target_);
+    }
+
+    function _installModule(address newModule_) internal {
+        bytes3 keycode = Module(newModule_).KEYCODE();
+
+        // @NOTE check newModule_ != 0
+        if (getModuleForKeycode[keycode] != address(0))
+            revert Kernel_ModuleAlreadyInstalled(keycode);
+
+        getModuleForKeycode[keycode] = newModule_;
+        getKeycodeForModule[newModule_] = keycode;
+    }
+
+    function _upgradeModule(address newModule_) internal {
+        bytes3 keycode = Module(newModule_).KEYCODE();
+        address oldModule = getModuleForKeycode[keycode];
+
+        if (oldModule == address(0) || oldModule == newModule_)
+            revert Kernel_ModuleAlreadyExists(keycode);
+
+        getKeycodeForModule[oldModule] = bytes3(0);
+        getKeycodeForModule[newModule_] = keycode;
+        getModuleForKeycode[keycode] = newModule_;
+
+        _reconfigurePolicies();
+    }
+
+    function _approvePolicy(address policy_) internal {
+        if (approvedPolicies[policy_] == true)
+            revert Kernel_PolicyAlreadyApproved(policy_);
+
+        approvedPolicies[policy_] = true;
+
+        allPolicies.push(policy_);
+        Policy(policy_).configureModules();
+    }
+
+    function _terminatePolicy(address policy_) internal {
+        if (approvedPolicies[policy_] == false)
+            revert Kernel_PolicyNotApproved(policy_);
+
+        approvedPolicies[policy_] = false;
+    }
+
+    function _reconfigurePolicies() internal {
+        for (uint256 i = 0; i < allPolicies.length; i++) {
+            address policy_ = allPolicies[i];
+            if (approvedPolicies[policy_]) {
+                Policy(policy_).configureModules();
+            }
+        }
+    }
+}

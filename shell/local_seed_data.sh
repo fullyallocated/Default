2aaf8346619d2fdf155ad8d4f266cc6f9d7a8b32
zaqk <10659278+zaqk@users.noreply.github.com>
2022-06-26 00:32:01 -0400

    shell script for seed script

---
+++ shell/local_seed_data.sh
@@ -0,0 +1,2 @@
+# must be ran from project home directory ex. sh shell/deploy_local.sh
+forge script ./src/scripts/SeedData.s.sol:SeedData --fork-url http://localhost:8545  --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast -vvvv
